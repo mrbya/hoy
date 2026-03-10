b339e40763abecc76240f942078a0836d1001dcb
mrbya <v.toth.svk2@gmail.com>
2026-03-10 10:30:07 +0100

    Added initial client command plumbing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
+use clap::Args;
 use clap::Parser;
+use clap::Subcommand;
 
 #[derive(Debug, Parser)]
-struct Args {
+struct Cli {
     #[arg(short = 'p', long = "port", default_value_t = 7777)]
     port: u16,
 }
 
+#[derive(Subcommand, Debug, Clone)]
+pub enum Command {
+    Client,
+}
+
+#[derive(Args, Debug, Default, Clone)]
+pub struct ClientArgs;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
+    let args = Cli::parse();
     let addr = SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::LOCALHOST), args.port);
 
     hoy_net::server::run_server(addr).await?;
